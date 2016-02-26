5f8dcf861e0f018c527520c91f2e234f966861d8
Kishan B <b.kishan.n@gmail.com>
2016-02-26 02:19:22 +0000

    feat(jury_test): Passed all tests in jury test.

---
+++ lib/jury.rb
@@ -0,0 +1,36 @@
+class Jury
+  attr_accessor :members
+  def initialize()
+    @members = []
+  end
+  def add_member(member)
+    @members << member
+  end
+  def cast_votes(finalists)
+    votes = {}
+    finalists.each do |finalist|
+      votes[finalist] = 0
+    end
+    @members.each do |member|
+      selected_finalist = finalists[rand(2)]
+      votes[selected_finalist] = votes[selected_finalist] + 1
+      puts "#{member} cast their vote for #{selected_finalist}."
+    end
+    votes
+  end
+  def report_votes(votes)
+    votes.each { |k,v| puts "#{k} received #{v} votes."}
+  end
+  def announce_winner(final_votes)
+    max_votes = 0
+    winner = NIL
+    final_votes.each do |k,v|
+      if v > max_votes then
+        winner = k
+        max_votes = v
+      end 
+    end
+    puts "The winner is #{winner}."
+    winner
+  end
+end
